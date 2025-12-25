bee8b28366377448fac91efed30897c0049f114f
taoudi-abdelbasset <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-12-25 17:37:06 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/hello')
+def hello():
+    return {'message': 'Hello from Flask!'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
